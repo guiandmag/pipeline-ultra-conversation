792472de8485568174c0fc0245e2f6275aa57de6
Luis <luis.hansen@usp.br>
2017-10-04 16:42:50 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+
+const server = require('./app');
+const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server running on port: %d', port);
+});
